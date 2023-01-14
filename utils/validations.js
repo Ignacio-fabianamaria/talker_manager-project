70daebf37e4c2a36fc13c2c52a36616431ce742c
Siigmaf <faby.tbe@gmail.com>
2023-01-14 19:08:17 -0300

    corrigi validação password

--- utils/validations.js
+++ utils/validations.js
@@ -44,8 +44,10 @@ const validateEmail = async (req, res, next) => {
 const validatePassword = (req, res, next) => {
     const { password } = req.body;
 
-    if (!password) res.status(400).json({ message: 'O campo "password" é obrigatório' });
-   
+    if (password === undefined) {
+        res.status(400).json({ message: 'O campo "password" é obrigatório' });
+    }
+    
     if (password.length < 6) {
         return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
     }
